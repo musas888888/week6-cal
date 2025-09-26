78fd320a5c8004293522c5b2eb8b2a38984f8d91
musas888888 <musasadiqmusa@gmail.com>
2025-09-26 14:14:09 +0300

    update

--- src/main/java/Cal.java
+++ src/main/java/Cal.java
@@ -1,15 +1,22 @@
 public class Cal {
-    public int addMe(int a, int b) {
-        return a + b;
+
+    // --- käynnistyspiste ---
+    public static void main(String[] args) {
+        Cal c = new Cal();
+        System.out.println("Hello from Cal!");
+        System.out.println("2 + 3 = " + c.addMe(2, 3));
+        System.out.println("7 - 5 = " + c.subMe(7, 5));
+        System.out.println("4 * 6 = " + c.mulMe(4, 6));
+        System.out.println("8 / 2 = " + c.divMe(8, 2));
     }
-    public int subMe(int a, int b) {
-        return a - b;
-    }
-    public int mulMe(int a, int b) {
-        return a * b;
-    }
+
+    // --- logiikka ---
+    public int addMe(int a, int b) { return a + b; }
+    public int subMe(int a, int b) { return a - b; }
+    public int mulMe(int a, int b) { return a * b; }
     public int divMe(int a, int b) {
         if (b == 0) throw new ArithmeticException("Division by zero");
-        else return a / b;
+        return a / b;
     }
 }
+
