bd295d1f382f22740afdd112e0fbd98cccb53e9b
musas888888 <musasadiqmusa@gmail.com>
2025-09-30 21:20:47 +0300

    updatet

--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -1,26 +1,46 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalTest {
-Cal cal=new Cal();
-    @Test
-    void addMe() {
-        assertEquals(12, cal.addMe(9,3));
+public class CalTest {
+
+    private Cal cal;
+
+    @BeforeEach
+    void setup() {
+        cal = new Cal();
     }
 
     @Test
-    void subMe() {
-        assertEquals(6, cal.subMe(9,3));
+    void addMe_addsTwoNumbers() {
+        assertEquals(5, cal.addMe(2, 3));
+        assertEquals(0, cal.addMe(-2, 2));
+        assertEquals(-7, cal.addMe(-3, -4));
     }
 
     @Test
-    void mulMe() {
-        assertEquals(27, cal.mulMe(9,3));
+    void subMe_subtractsTwoNumbers() {
+        assertEquals(2, cal.subMe(7, 5));
+        assertEquals(-4, cal.subMe(-2, 2));
+        assertEquals(1, cal.subMe(-3, -4));
     }
 
     @Test
-    void divMe() {
-        assertEquals(3, cal.divMe(9,3));
+    void mulMe_multipliesTwoNumbers() {
+        assertEquals(24, cal.mulMe(4, 6));
+        assertEquals(-8, cal.mulMe(-4, 2));
+        assertEquals(0, cal.mulMe(0, 999));
+    }
+
+    @Test
+    void divMe_dividesTwoNumbers() {
+        assertEquals(4, cal.divMe(8, 2));
+        assertEquals(-3, cal.divMe(-9, 3));
+    }
+
+    @Test
+    void divMe_throwsWhenDividingByZero() {
+        assertThrows(ArithmeticException.class, () -> cal.divMe(10, 0));
     }
 }
